5bb481a20a597db0848a2030832d789431f57648
tanny <94848716+ilovespectra@users.noreply.github.com>
2023-11-11 16:32:38 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,29 +2,43 @@ import { Keypair } from "@solana/web3.js";
 import { SendTransactionRequest } from "components/SendTransactionRequest";
 import { TransactionRequestQR } from "components/TransactionRequestQR";
 import useTransactionListener from "hooks/useTransactionListener";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import type { NextPage } from "next";
 import { useMemo } from "react";
 
 const Home: NextPage = () => {
-  // Generate a public key that will be added to the transaction
-  // so we can listen for it
   const reference = useMemo(() => Keypair.generate().publicKey, []);
-
-  // Listen for transactions with the reference
   useTransactionListener(reference);
 
   return (
-    <div className="hero rounded-2xl bg-base-content">
-      <div className="hero-content text-center">
-        <div className="max-w-lg flex flex-col gap-6">
-          <h1 className="text-3xl font-bold text-primary">Transaction</h1>
-          {/* Button to send a transaction request */}
-          <SendTransactionRequest reference={reference} />
-          {/* QR code for a transaction request */}
-          <TransactionRequestQR reference={reference} />
+    <Router>
+      <div className="hero rounded-2xl bg-base-content">
+        <div className="hero-content text-center">
+          <div className="max-w-lg flex flex-col gap-6">
+            <h1 className="text-3xl font-bold text-primary">Transaction</h1>
+
+            {/* Define your routes */}
+            <Routes>
+              <Route path="/send">
+                {/* Component for sending transaction request */}
+                <SendTransactionRequest reference={reference} />
+              </Route>
+              <Route path="/qr">
+                {/* Component for QR code */}
+                <TransactionRequestQR reference={reference} />
+              </Route>
+              <Route path="/">
+                {/* Default component, e.g., Home page */}
+                {/* Button to send a transaction request */}
+                <SendTransactionRequest reference={reference} />
+                {/* QR code for a transaction request */}
+                <TransactionRequestQR reference={reference} />
+              </Route>
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
