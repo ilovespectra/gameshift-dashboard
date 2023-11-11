0d651fb356fcb68f148374224e02a136193d1b54
tanny <94848716+ilovespectra@users.noreply.github.com>
2023-11-11 16:41:13 -0700

    Update fetchFirestoreUsers.tsx

--- src/components/fetchFirestoreUsers.tsx
+++ src/components/fetchFirestoreUsers.tsx
@@ -1,18 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore';
-import { firebaseConfig } from './firebase'; 
+import { firebaseConfig } from './firebase'; // Make sure you have firebaseConfig defined in the 'firebase' file
 import { PublicKey } from '@solana/web3.js';
+
+// Initialize Firebase if not already initialized
+const firebaseApp = initializeApp(firebaseConfig);
 
 const fetchFirestoreUsers = async (walletPublicKey: PublicKey) => {
   try {
-    // Initialize Firebase if not already initialized
-    if (firebase.getApps().length === 0) {
-      initializeApp(firebaseConfig); // Replace with your actual Firebase configuration
-    }
-
     // Fetch users from Firestore based on the wallet's public key
-    const usersCollection = collection(getFirestore(), 'users');
-    const usersQuery = query(usersCollection, where('publicKey', '==', walletPublicKey));
+    const usersCollection = collection(getFirestore(firebaseApp), 'users');
+    const usersQuery = query(usersCollection, where('publicKey', '==', walletPublicKey.toString()));
     const usersSnapshot = await getDocs(usersQuery);
 
     // Map the user data from Firestore documents
