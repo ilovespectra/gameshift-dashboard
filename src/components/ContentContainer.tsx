43ef94eacecbfbae98893780faaa77b60176cb75
tanny <94848716+ilovespectra@users.noreply.github.com>
2023-11-11 16:55:05 -0700

    Update ContentContainer.tsx

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -1,7 +1,14 @@
 import { FC } from 'react';
-import Link from "next/link";
+import Link from 'next/link';
 
-export const ContentContainer: FC = props => {
+export const ContentContainer: FC = (props) => {
+  const handleHomeLinkClick = () => {
+    // Your logic before reloading the page
+    // ...
+
+    // Reload the page
+    window.location.reload();
+  };
   return (
     <div className="flex-1 drawer h-52">
       {/* <div className="h-screen drawer drawer-mobile w-full"> */}
@@ -15,12 +22,15 @@ export const ContentContainer: FC = props => {
         <label htmlFor="my-drawer" className="drawer-overlay"></label>
         <ul className="p-4 overflow-y-auto menu w-80 bg-base-100">
           <li>
-          <Link href="https://github.com/ilovespectra/gameshift-dashboard">
-            <a target="_blank" rel="noopener noreferrer">Github</a>
-          </Link>
+            <a onClick={handleHomeLinkClick}>Home</a>
           </li>
           <li>
-            <Link href="https://www.solarplex.xyz/profile/tanny.live.solarplex.xyz">
+            <Link href="https://github.com/ilovespectra/solo">
+              <a target="_blank" rel="noopener noreferrer">GitHub</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://www.solarplex.xyz/profile/solo.live.solarplex.xyz">
               <a target="_blank" rel="noopener noreferrer">Solarplex</a>
             </Link>
           </li>
